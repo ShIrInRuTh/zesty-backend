b46b19ce4f43cc5c1d0a50a94b872d7dec1ee64f
WilliamsSmithM <mw.smith.2024@computing.smu.edu.sg>
2025-10-25 00:23:22 +0800

    [W][help]

--- routes/routers.js
+++ routes/routers.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const authRoutes = require("./authRoutes");
+const fridgeRoutes = require("./fridgeRoutes");
 
 // Mount auth routes at /api/auth (if have any)
 router.use("/auth", authRoutes);
+router.use("/fridge", fridgeRoutes);
 // Healthcheck route at /api/healthcheck
 router.get("/healthcheck", (req, res) => {
   res.status(200).send({ status: "ok" });
